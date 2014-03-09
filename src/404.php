19555ae22641ca806f49cc7f784f801ef2824d93
Andrew Carreiro <andrew@headfirst.ca>
2014-03-09 11:00:14 -0400

    404 page

---
+++ src/404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+<div class="content">
+	<div class="post">
+		<article>
+			<div class="postcontent fourohfour">
+				<div class="deadlink"></div>
+				<div>
+					<h1>Looks like you came across a dead Link.</h1>
+					<p>Should something be here? <a href="mailto:admin@1morecastle.com">Email us.</a></p>
+				</div>
+			</div>
+		</article>
+	</div>
+</div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
