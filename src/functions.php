bbd733093584fd34304e35d0c72b668b019d913a
Andrew Carreiro <andrew@headfirst.ca>
2014-03-17 22:46:01 -0400

    analytics fixes

--- src/functions.php
+++ src/functions.php
@@ -311,8 +311,7 @@
 		if(!current_user_can('edit_post')){
 			$analyticsString .= "<script>";
 			
-			$analyticsString = $analyticsString."(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');";
-			$analyticsString = $analyticsString."function ga();";
+			$analyticsString = $analyticsString."(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-30208407-1', '1morecastle.com');ga('send', 'pageview');";
 			$analyticsString .= "ga('create', 'UA-30208407-1', '1morecastle.com');ga('send', 'pageview');";
 
 			if(have_posts()) : while(have_posts()) : the_post();
@@ -347,6 +346,7 @@
 				}else{
 					//page type
 					$analyticsString .= "ga('set', 'dimension4', 'multi');";
+					break;
 				}
 			
 			endwhile; endif;
