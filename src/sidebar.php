72ab8f891d2e6c49476c66a26386f9d6d36a0d60
Andrew Carreiro <andrew@headfirst.ca>
2014-06-03 20:51:04 -0400

    removed Indy Popcon element

--- src/sidebar.php
+++ src/sidebar.php
@@ -1,13 +1,5 @@
 <aside class="sidebar">
 	<div>
-		<h3>Indy PopCon</h3>
-		<div>
-			<a href="http://1morecastle.com/2014/02/meet-us-at-indy-popcon/">
-				<img src="http://1morecastle.com/wp-content/uploads/2014/02/indypopcon.jpg">
-			</a>
-		</div>
-	</div>
-	<div>
 		<h3>Get Involved at 1 More Castle!</h3>
 		<div>
 			<a href="<?php bloginfo('url'); ?>/contribution-guidelines/">
