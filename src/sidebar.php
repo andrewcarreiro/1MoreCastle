fb2e3ac724900587775ca1a2be898cd8ade51eaa
Andrew Carreiro <andrew@headfirst.ca>
2014-06-16 21:27:02 -0400

    new wave traders ad

--- src/sidebar.php
+++ src/sidebar.php
@@ -9,9 +9,18 @@
 	</div>
 	<?php omc_featured_series(); ?>
 
-	<div>
+	<div class="zad">
 		<div>
 			<?php omc_get_ad('sidebar'); ?>
+		</div>
+	</div>
+
+	<div>
+		<h3>Visit our friends:</h3>
+		<div>
+			<a href="http://www.newwavetraders.com/" target="_blank">
+				<img src="<?php bloginfo('template_url'); ?>/images/featured-sidebar/nwt2.jpg"/>
+			</a>
 		</div>
 	</div>
 	
