68de679bddc82475a010b03c718c23131c2927b5
Andrew Carreiro <andrew@headfirst.ca>
2015-06-02 21:44:45 -0400

    removed get involved from sidebar

--- src/sidebar.php
+++ src/sidebar.php
@@ -6,14 +6,6 @@
 			</a>
 		</div>
 	</div>
-	<div>
-		<h3>Get Involved at 1 More Castle!</h3>
-		<div>
-			<a href="<?php bloginfo('url'); ?>/contribution-guidelines/">
-				<img src="<?php bloginfo('template_url'); ?>/images/cta/contribute_<?php echo rand(1,3); ?>.png"/>
-			</a>
-		</div>
-	</div>
 	<?php omc_featured_series(); ?>
 
 	<div class="zad">
